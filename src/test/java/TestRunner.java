6294472052c7cba309dfa6d62d3905b73ddcea25
santosh <santosh.tallapudi@gmail.com>
2021-05-30 14:06:10 +0100

    Test

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -3,6 +3,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
+
 @CucumberOptions(features = "src/test/java/features",
                  //glue = "src/test/java/stepDefinitions",
                  //tags="@Before or @SmokeTest",
